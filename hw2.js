2480700318fc62d603ea6d1a937cefc213314461
Misha <suxx_2000@hotmail.com>
2017-09-24 01:31:28 +0300

    hw2 done

--- hw2.js
+++ hw2.js
@@ -23,7 +23,13 @@
     const getOneObject = (...obj) => {
         return Object.assign(...obj);
     };
-    getOneObject( {test: 123}, {value: 'hello'}, {flag: false} );
+    getOneObject({
+        test: 123
+    }, {
+        value: 'hello'
+    }, {
+        flag: false
+    });
 
     // task 5.1
     class Component {
@@ -36,28 +42,106 @@
     //task 5.2
     class Component2 {
         constructor(tagName = 'div') {
-            this.node = document.createElement(tagName); 
+            this.node = document.createElement(tagName);
         }
-        setText(text) { 
+        setText(text) {
             this.node.textContent = text;
-        }  
+        }
     }
     // task 5.3
     class Component3 {
-        constructor(tagName = 'div'){
+        constructor(tagName = 'div') {
             this.node = document.createElement(tagName);
         }
-        setText(text) {this.node.textContent = text;}
+        setText(text) {
+            this.node.textContent = text;
+        }
     }
     class InputComponent extends Component3 {
         constructor(tagName) {
             super(tagName);
             this.node.placeholder = 'Enter text';
         }
-        setText(text) { this.node.value = text; }
-        
+        setText(text) {
+            this.node.value = text;
+        }
+
     }
     // task 5.4 
-    
-
+    class Figure {
+        constructor(angles = 1) {
+            this.abstract = true;
+            this.angles = angles;
+        }
+        getInfo() {
+            return {
+                angles: this.angles,
+                abstract: this.abstract
+            };
+        }
+    }
+    class Triangle extends Figure {
+        constructor(angles, name = 'triangle') {
+            super(angles);
+            this.abstract = false;
+            this.name = name;
+        }
+        getInfo() {
+            const props = super.getInfo();
+            props.name = this.name;
+            return props;
+        }
+    }
+    // task 5.5
+    class Table {
+        constructor(rows = 3, columns = 3) {
+            this.rows = rows;
+            this.columns = columns;
+            this.matrix = (function () {
+                let result = [];
+                for (let i = 0; i < rows; i++) {
+                    let temp = [];
+                    result.push(temp);
+                    for (let j = 0; j < columns; j++) {
+                        let temp2 = [{
+                            text: ''
+                        }];
+                        result[i].push(temp2);
+                    }
+                }
+                return result;
+            })();
+        }
+        getCells() {
+            return this.rows * this.columns;
+        }
+        setCellText(row, column, text) {
+            this.matrix[row][column].text = text;
+        }
+        getCellText(row, column) {
+            return this.matrix[row][column].text;
+        }
+        getTableInfo() {
+            return {
+                cells: this.getCells(),
+                rows: this.rows,
+                columns: this.columns
+            };
+        }
+        static calcCells(rows, columns) {
+            return rows * columns;
+        }
+    }
+    // task 5.6
+    class heirTable extends Table {
+        constructor(rows, columns, name = 'table') {
+            super(rows, columns);
+            this.name = name;
+        }
+        getTableInfo() {
+            let result = super.getTableInfo();
+            result.name = this.name;
+            return result;
+        }
+    }
 })();
